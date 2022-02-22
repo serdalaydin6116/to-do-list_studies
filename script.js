dbe21c0aa6ee1f45b70ff4462fdee80110165f9f
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-02-23 02:16:21 +0300

    worked on edit scripts

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 let task = document.getElementById("task");
 let plus = document.getElementById("plus");
 let list = document.getElementById("list");
+// console.log(list);
+// console.log(task);
+let input = document.querySelectorAll(".task-input");
+// console.log(input);
 
 
 
 
-// const completedTaskCount = () =>{
+// // // // const completedTaskCount = () =>{
   
 
   
-//     let checkedCount = list.getElementsByClassName("control").length
-//     console.log(checkedCount);
-//     let taskCount = document.getElementsByClassName("myInput").length
-//     console.log(taskCount);
+//    let checkedCount = list.getElementsByClassName("control").length
+//    console.log(checkedCount);
+//    let taskCount = document.getElementsByClassName("myInput").length
+//    console.log(taskCount);
   
-//     console.log(checkedCount, taskCount);
-//     const checkedTasks =document.getElementById("checkedTasks"); 
-//     const allTasks =document.getElementById("allTasks");
-//     checkedTasks.innerText = checkedCount;
+//    console.log(checkedCount, taskCount);
+//    const checkedTasks =document.getElementById("checkedTasks"); 
+//    const allTasks =document.getElementById("allTasks");
+//    checkedTasks.innerText = checkedCount;
 //     allTasks.innerText = taskCount;
 
-// }
+//  }
 
 
 
@@ -64,18 +68,22 @@ plus.addEventListener("click", () => {
     let btndelete = document.getElementById("trash")
     btndelete.addEventListener("click",()=>{
         new_list.remove()    
-    })
+    });
 
-    let btnedit = document.getElementById("edit")
-    btnedit.addEventListener("click", ()=>{
-        let content = btnedit.parentElement
-        
-        if (content.getAttribute("contenteditable") == "true"){
-            content.setAttribute("contenteditable", false)
-        } else {
-            content.setAttribute("contenteditable", true)
-        }
-    })
+//     console.log(new_list);
+
+    // let btnedit = document.getElementById("edit");
+    // console.log(btnedit);
+
+
+    // btnedit.addEventListener("click", (e)=>{      
+    //     let content = e.target.parentElement.parentElement.children[1]
+    //     console.log(content);  
+    //     // if (content != ""){
+    //     //     content.remove()
+            
+    //     // } 
+    // })
 
 
     let btncheck = document.getElementById("myCheckbox")
